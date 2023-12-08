8893bf18ded3703a50b6d2230200275c44ca1fc0
Ujjwal Suman <ujjwalsuman007@gmail.com>
2023-12-08 09:00:43 +0530

    commit1
    Post, Get, Put Operation In blog Post

---
+++ config/database.js
@@ -0,0 +1,23 @@
+//its work is to established connection b/w application and db
+
+
+const mongoose = require("mongoose");
+
+//load configration of env file to process object
+require("dotenv").config();
+
+const connectWithDb = () => {
+    mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser:true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB ka Connection is Successful"))
+    .catch( (error) => {
+        console.log("Issue in DB Connection");
+        console.error(error.message);
+        //iska matlab kya h ?
+        process.exit(1);
+    } );
+}
+
+module.exports = connectWithDb;
